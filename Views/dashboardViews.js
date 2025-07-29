5059b4a93a2b65a8fbed6295a1133486f6da6754
gemlmc <gmluch@scl.edu.gt>
2025-07-29 12:53:26 -0600

    frontEnd footer

--- Views/dashboardViews.js
+++ Views/dashboardViews.js
@@ -1,10 +1,12 @@
 /* alert("Hola Mundo"); */ 
 import { header } from "../components/header/headerComponent.js";
+import { footer } from "../components/footer/footerComponent.js";
 
 export function dashboard(){
     let seccion = document.createElement('section');
 
     seccion.appendChild(header());
+    seccion.appendChild(footer());
    /*  let h1 = document.createElement('h1');
     h1.innerText = "hola mundo"
     seccion.appendChild(h1); */
