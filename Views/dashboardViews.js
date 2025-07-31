92b29a888e99e3d47f3696a73d8948bb31a8cac2
gemlmc <gmluch@scl.edu.gt>
2025-07-31 08:22:35 -0600

    se realizan cambios

--- Views/dashboardViews.js
+++ Views/dashboardViews.js
@@ -1,18 +1,48 @@
 /* alert("Hola Mundo"); */ 
-import { header } from "../components/header/headerComponent.js";
+/* import { header } from "../components/header/headerComponent.js";
 import { footer } from "../components/footer/footerComponent.js";
 
 export function dashboard(){
     let seccion = document.createElement('section');
 
     seccion.appendChild(header());
-    seccion.appendChild(footer());
+    seccion.appendChild(footer()); */
    /*  let h1 = document.createElement('h1');
     h1.innerText = "hola mundo"
     seccion.appendChild(h1); */
 
 
-    return seccion;
+  /*   return seccion;
+}
+
+document.body.appendChild(dashboard()); */
+
+import { header } from "../components/header/headerComponent.js";
+import { tareas } from "../components/tareas/tareasComponent.js";
+import { informacion } from "../components/informacion/informacionComponent.js";
+import { footer } from "../components/footer/footerComponent.js";
+
+export function dashboard(){
+
+    let dashboard = document.createElement('section');
+    dashboard.className = "dashboard";
+
+    //header
+    dashboard.appendChild(header());
+
+    //seccion1
+    let seccion1 = document.createElement('section');
+    seccion1.className = "seccion-1";
+
+    seccion1.appendChild(tareas());
+    seccion1.appendChild(informacion());
+
+    dashboard.appendChild(seccion1);
+
+    //footer
+    dashboard.appendChild(footer());
+
+    return dashboard;
 }
 
 document.body.appendChild(dashboard());
